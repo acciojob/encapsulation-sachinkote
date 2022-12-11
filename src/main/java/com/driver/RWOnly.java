bb56c941c41557fc73e9d6b4186f778219aacfe4
SAKHARAM KOTE <sachin.sakharam-kote@capgemini.com>
2022-12-11 15:48:38 +0530

    encapsulation assignment

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,15 @@ package com.driver;
 
 public class RWOnly {
     
+	private  String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
